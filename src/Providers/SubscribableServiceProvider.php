f8aa6b9f60a66a9fa0330bf356765a49fc3e2fb6
Abdelrahman Omran <me@omranic.com>
2017-08-20 14:09:49 +0200

    Bind model alias into IoC container

--- src/Providers/SubscribableServiceProvider.php
+++ src/Providers/SubscribableServiceProvider.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Rinvex\Subscribable\Providers;
 
+use Rinvex\Subscribable\Models\Plan;
 use Illuminate\Support\ServiceProvider;
+use Rinvex\Subscribable\Models\PlanFeature;
+use Rinvex\Subscribable\Models\PlanSubscription;
+use Rinvex\Subscribable\Models\PlanSubscriptionUsage;
 use Rinvex\Subscribable\Console\Commands\MigrateCommand;
 
 class SubscribableServiceProvider extends ServiceProvider
@@ -27,22 +31,26 @@ class SubscribableServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(realpath(__DIR__.'/../../config/config.php'), 'rinvex.subscribable');
 
-        // Register eloquent models
+        // Bind eloquent models to IoC container
         $this->app->singleton('rinvex.subscribable.plan', function ($app) {
             return new $app['config']['rinvex.subscribable.models.plan']();
         });
+        $this->app->alias('rinvex.subscribable.plan', Plan::class);
 
         $this->app->singleton('rinvex.subscribable.plan_features', function ($app) {
             return new $app['config']['rinvex.subscribable.models.plan_features']();
         });
+        $this->app->alias('rinvex.subscribable.plan_features', PlanFeature::class);
 
         $this->app->singleton('rinvex.subscribable.plan_subscriptions', function ($app) {
             return new $app['config']['rinvex.subscribable.models.plan_subscriptions']();
         });
+        $this->app->alias('rinvex.subscribable.plan_subscriptions', PlanSubscription::class);
 
         $this->app->singleton('rinvex.subscribable.plan_subscription_usage', function ($app) {
             return new $app['config']['rinvex.subscribable.models.plan_subscription_usage']();
         });
+        $this->app->alias('rinvex.subscribable.plan_subscription_usage', PlanSubscriptionUsage::class);
 
         // Register artisan commands
         foreach ($this->commands as $key => $value) {
