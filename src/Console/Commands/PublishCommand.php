759fe81d7fab1b2e486f6b8961574041863dd05b
Abdelrahman Omran <me@omranic.com>
2020-03-30 05:51:33 +0400

    Enforce consistent artisan command tag namespacing

--- src/Console/Commands/PublishCommand.php
+++ src/Console/Commands/PublishCommand.php
@@ -33,14 +33,14 @@ class PublishCommand extends Command
 
         switch ($this->option('resource')) {
             case 'config':
-                $this->call('vendor:publish', ['--tag' => 'rinvex-subscriptions-config', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'rinvex/subscriptions::config', '--force' => $this->option('force')]);
                 break;
             case 'migrations':
-                $this->call('vendor:publish', ['--tag' => 'rinvex-subscriptions-migrations', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'rinvex/subscriptions::migrations', '--force' => $this->option('force')]);
                 break;
             default:
-                $this->call('vendor:publish', ['--tag' => 'rinvex-subscriptions-config', '--force' => $this->option('force')]);
-                $this->call('vendor:publish', ['--tag' => 'rinvex-subscriptions-migrations', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'rinvex/subscriptions::config', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'rinvex/subscriptions::migrations', '--force' => $this->option('force')]);
                 break;
         }
 
